fffe2e0f9f207fd119dd270e5e7299912d93e6ba
Supriya Kshirsagar <supriyakshirsagar9@gmail.com>
2021-04-17 19:17:56 -0400

    All CURD operation on Book table with Entity framework core and razor pages in .Net core 3.1

---
+++ BookApp/Controlles/BookController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookApp.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApp.Controlles
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public BookController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetALL()
+        {
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var fromDB = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (fromDB == null)
+            {
+                return Json(new { success = false, message = "Error while deleting record" });
+            }
+            _db.Book.Remove(fromDB);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+    }
+}
--- BookApp/Startup.cs
+++ BookApp/Startup.cs
@@ -26,6 +26,7 @@ namespace BookApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDBContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
@@ -52,6 +53,7 @@ namespace BookApp
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
